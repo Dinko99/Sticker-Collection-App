4c365df6f7c19263787d9ffed36934e5a9b156e8
Dinko99 <dinkodin99@gmail.com>
2022-09-07 02:24:16 +0200

    testing

--- client/src/components/AllStickers.jsx
+++ client/src/components/AllStickers.jsx
@@ -1,65 +1,78 @@
-import '../Styles/AllStickers.scss'
-import CountryTitle from './CountryTitle'
-import StickerNumber from './StickerNumber'
-import stickers from '../utils/stickers'
-import fwcFront from '../utils/fwcFront'
-import fwcBack from '../utils/fwcBack'
-import cocaCola from '../utils/cocaCola'
+import '../Styles/AllStickers.scss';
+import CountryTitle from './CountryTitle';
+import StickerNumber from './StickerNumber';
+import stickers from '../utils/stickers';
+import fwcFront from '../utils/fwcFront';
+import fwcBack from '../utils/fwcBack';
+import cocaCola from '../utils/cocaCola';
 
-
-
-const AllStickers = ({countries, setStickersCollected}) => {
-    const [number, collected] = stickers;
-    
+const AllStickers = ({ countries, setStickersCollected }) => {
   return (
     <main>
-        <div className="stickers-section">
-            <CountryTitle title='fwc' />
-            <div className="stickers">
-                {
-                    fwcFront.map((number, index)=>{
-                        return <StickerNumber key={index} number={number} setStickersCollected={setStickersCollected} />
-                    })
-                }
-            </div>
+      <div className='stickers-section'>
+        <CountryTitle title='fwc' />
+        <div className='stickers'>
+          {fwcFront.map((number, index) => {
+            return (
+              <StickerNumber
+                stickers={stickers}
+                s
+                key={index}
+                number={number}
+                setStickersCollected={setStickersCollected}
+              />
+            );
+          })}
         </div>
-        {
-            countries.map((country, index)=>{
-                return <div key={index} className="stickers-section">
-                    <CountryTitle  title={country} />
-                    <div className="stickers">
-                {
-                    stickers.map((item, index)=>{
-                        return <StickerNumber key={index} number={item.number} setStickersCollected={setStickersCollected}   />
-                    })
-                }
-                    </div>
-                </div>
-            })
-        }
-         <div className="stickers-section">
-            <CountryTitle title='fwc' />
-            <div className="stickers">
-                {
-                    fwcBack.map((number, index)=>{
-                        return <StickerNumber key={index} number={number} setStickersCollected={setStickersCollected} />
-                    })
-                }
+      </div>
+      {countries.map((country, index) => {
+        return (
+          <div key={index} className='stickers-section'>
+            <CountryTitle title={country} />
+            <div className='stickers'>
+              {stickers.map((item, index) => {
+                return (
+                  <StickerNumber
+                    key={index}
+                    number={item}
+                    setStickersCollected={setStickersCollected}
+                  />
+                );
+              })}
             </div>
+          </div>
+        );
+      })}
+      <div className='stickers-section'>
+        <CountryTitle title='fwc' />
+        <div className='stickers'>
+          {fwcBack.map((number, index) => {
+            return (
+              <StickerNumber
+                key={index}
+                number={number}
+                setStickersCollected={setStickersCollected}
+              />
+            );
+          })}
         </div>
-        <div className="stickers-section">
-            <CountryTitle title='cocacola' />
-            <div className="stickers">
-                {
-                    cocaCola.map((number, index)=>{
-                        return <StickerNumber key={index} number={number} setStickersCollected={setStickersCollected} />
-                    })
-                }
-            </div>
+      </div>
+      <div className='stickers-section'>
+        <CountryTitle title='cocacola' />
+        <div className='stickers'>
+          {cocaCola.map((number, index) => {
+            return (
+              <StickerNumber
+                key={index}
+                number={number}
+                setStickersCollected={setStickersCollected}
+              />
+            );
+          })}
         </div>
+      </div>
     </main>
-    
-  )
-}
+  );
+};
 
-export default AllStickers
+export default AllStickers;
--- client/src/components/StickerNumber.jsx
+++ client/src/components/StickerNumber.jsx
@@ -1,29 +1,35 @@
-import { useState, useEffect } from "react";
-import {FcCheckmark} from 'react-icons/fc'
+import { useState, useEffect } from 'react';
+import { FcCheckmark } from 'react-icons/fc';
 
-const StickerNumber = ({number, setStickersCollected}) => {
-  const [isCollected, setIsCollected] = useState(false)
-  
-  useEffect(()=>{
-  }, [isCollected])
+const StickerNumber = ({ number, setStickersCollected }) => {
+  const [isCollected, setIsCollected] = useState(
+    JSON.parse(localStorage.getItem('isCollected')) || false
+  );
 
-  const handleClick = (e) => { 
-    setIsCollected(!isCollected)    
-    if(isCollected === false){
-      setStickersCollected((prev) => prev + 1)
-      
+  useEffect(() => {
+    window.localStorage.setItem('isCollected', JSON.stringify(isCollected));
+  }, [isCollected]);
+
+  const handleClick = (e) => {
+    setIsCollected(!isCollected);
+    if (isCollected === false) {
+      setStickersCollected((prev) => prev + 1);
+    } else if (isCollected === true) {
+      setStickersCollected((prev) => prev - 1);
     }
-    else if(isCollected === true){
-      setStickersCollected((prev) => prev - 1)
-    }
-    
-  }
-
- 
+  };
 
   return (
-    <p className='sticker-number' onClick={handleClick} >{number} <span className="checkmark" style={{display: isCollected === false ? 'none' : 'block'}}><FcCheckmark /></span></p>
-  )
-}
+    <p className='sticker-number' onClick={handleClick}>
+      {number}
+      <span
+        className='checkmark'
+        style={{ display: isCollected === false ? 'none' : 'block' }}
+      >
+        <FcCheckmark />
+      </span>
+    </p>
+  );
+};
 
 export default StickerNumber;
